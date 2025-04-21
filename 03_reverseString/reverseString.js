fafb594df85ccc5316d8db5ddaff749cfdae96d4
Paul <54538759+eoSFwae@users.noreply.github.com.>
2025-04-21 14:14:06 -0400

    exercise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = []
+    for(let i = word.length - 1; i >= 0; i--) {
+        reversedWord.push(word[i])
+    }
+    return reversedWord.join('');
 };
+
+// console.log(reverseString("hello world"));
 
 // Do not edit below this line
 module.exports = reverseString;
