460f89ee87cf97a5b5b8136775313073f447d895
Paul <54538759+eoSFwae@users.noreply.github.com.>
2025-04-28 15:57:22 -0400

    exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0){
+        return "OOPS";
+    }
+    let fib = []
+    for (let i = 0; i <= number; i++) {
+        if( i === 0 || i === 1){
+            fib.push(i)
+        }
+        else{
+            let a = fib[i - 2]
+            let b = fib[i - 1]
+            fib.push(a + b)
+        }
 
+    }
+
+    return fib[number]
 };
 
 // Do not edit below this line
+
+console.log(fibonacci(6));
 module.exports = fibonacci;
