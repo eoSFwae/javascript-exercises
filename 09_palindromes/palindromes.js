d92d22da9047aa645daa0bd9b280d1974dc1ffc9
Paul <54538759+eoSFwae@users.noreply.github.com.>
2025-04-28 13:23:29 -0400

    exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let stripped = word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '').toLowerCase().split('');
+    let reversed = stripped.slice().reverse()
 
+    console.log(stripped)
+    console.log(reversed)
+
+    for(let index = 0; index < reversed.length; index++) {
+        if (!(reversed[index] === stripped[index])) {
+            return false
+        }
+
+    }
+    return true
 };
 
+
+console.log(palindromes('wow'));
 // Do not edit below this line
 module.exports = palindromes;
